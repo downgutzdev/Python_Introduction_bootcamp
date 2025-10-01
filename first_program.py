3f0e7b19d41a1e5cbd437c0f27b06a80c7366d73
downgutzdev <downgutzdev@gmail.com>
2025-10-01 19:59:12 -0300

    feat: add first_program.py with print examples using the sep parameter and notes on print function parameters

---
+++ first_program.py
@@ -0,0 +1,22 @@
+# first_program.py 
+
+'''
+The first file of the Python Bootcamp class on DIO
+The hashtag (#) is used to write comments
+Triple single quotes can also be used for multi-line comments
+Triple double quotes are used for docstrings
+'''
+
+"""
+Print is used to display information or the value of variables on the screen so you can see the program’s output.
+The print function has many parameters, such as sep (used to separate arguments) and end (used to specify what is printed at the end)
+Windows uses CRLF (\r\n) for line breaks
+"""
+
+print("Hello word!")
+
+print(12, 34, sep="-")
+
+print(56, 78, sep=" and ")
+
+print(1, 2, sep=",", end=".\n")
